a3c6692252b95f43ef1d2910783cacbc445e0d0b
joebeach <jlopez.mail@gmail.com>
2014-04-27 21:07:59 -0300

    a

--- servidor/server.py
+++ servidor/server.py
@@ -109,7 +109,7 @@ while(1):
 			errorgps=paquete[0][46:48]
 			#print "Fecha %s Hora GMT %s - Vehiculo %s - Tipo %s localizado en %s %s a %s kms/h con rumbo %s" % (fecha,hora,id_virloc,tipo,latitud,longitud,velocidad,rumbo)
 			DB.sqlInsertOrUpdate('equipos','id=%s,ip="%s",puerto=%s,latitud="%s",longitud="%s",velocidad=%s,rumbo=%s' % (id_virloc,host,port,latitud,longitud,velocidad,rumbo),'ip="%s",puerto=%s,latitud="%s",longitud="%s",velocidad=%s,rumbo=%s' % (host,port,latitud,longitud,velocidad,rumbo))
-			DB.sqlInsert('posiciones','id_virloc=%s,ip="%s",puerto=%s,latitud="%s",longitud="%s",velocidad=%s,rumbo=%s' % (id_virloc,host,port,latitud,longitud,velocidad,rumbo))
+			DB.sqlInsert('posiciones','id_virloc=%s,host="%s",port=%s,latitud="%s",longitud="%s",velocidad=%s,rumbo=%s' % (id_virloc,host,port,latitud,longitud,velocidad,rumbo))
 
 			evento=info[44:46]
 			if evento=="15":
